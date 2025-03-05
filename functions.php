7b42a83ce7dc3b26213155b0ce9a4f4959989917
ErickAmaralFreitasDev <123erikalluno123@gmail.com>
2025-03-05 17:49:52 -0300

    inicio

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+    remove_action('rest_api_init', 'create_initial_rest_routes', 99);
+    
+    $dirbase = get_template_directory();
+    require_once $dirbase . '/endpoints/user_post.php';
+?>
