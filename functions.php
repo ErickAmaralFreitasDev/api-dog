75282faf23189c7db1eae2ca8ff41b3d8e284017
ErickAmaralFreitasDev <123erikalluno123@gmail.com>
2025-03-12 18:14:14 -0300

    function expire

--- functions.php
+++ functions.php
@@ -3,4 +3,9 @@
     
     $dirbase = get_template_directory();
     require_once $dirbase . '/endpoints/user_post.php';
+
+    function expire_token () {
+        return time() + (60 * 60 * 24);
+    }
+    add_action('jwt_auth_expire', 'expire_token')
 ?>
