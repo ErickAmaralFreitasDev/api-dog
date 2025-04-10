453059b1fcb06ef34a16a83abd8086457b249a3f
Your Name <123erikalluno123@gmail.com>
2025-04-10 15:20:15 -0300

    photo delete

--- endpoints/photo_post.php
+++ endpoints/photo_post.php
@@ -3,21 +3,46 @@ function api_photo_post($request) {
     $user = wp_get_current_user();
     $user_id = $user -> ID;
 
-    if (user_id === 0) {
-        $response = new WP_Error('error', 'Usuário não possui permissão', [status => 401]);
+    if ($user_id === 0) {
+        $response = new WP_Error('error', 'Usuário não possui permissão', ['status' => 401]);
         return rest_ensure_response($response);
     }
 
     $nome = sanitize_text_field($request['nome']);
     $peso = sanitize_text_field($request['peso']);
     $idade = sanitize_text_field($request['idade']);
+    $files = $request->get_file_params(); 
 
-    if (empty($nome) || empty($peso) || empty($idade)) {
-        $response = new WP_Error('error', 'Dados incompletos', [status => 422]);
+    if (empty($nome) || empty($peso) || empty($idade) || empty($files)) {
+        $response = new WP_Error('error', 'Dados incompletos', ['status' => 422]);
         return rest_ensure_response($response); 
     }
 
-    return rest_ensure_response(3);
+    $response = [
+        'post_author' => $user_id,
+        'post_type' => 'post',
+        'post_status' => 'publish',
+        'post_title' => $nome,
+        'files' => $files,
+        'post_content' => $nome,
+        'meta_input' => [
+            'peso' => $peso,
+            'idade' => $idade,
+            'acessos' => 0,
+        ],
+    ];
+
+    $post_id = wp_insert_post($response);
+
+    require_once ABSPATH . 'wp-admin/includes/image.php';
+    require_once ABSPATH . 'wp-admin/includes/file.php';
+    require_once ABSPATH . 'wp-admin/includes/media.php';
+
+    $photo_id = media_handle_upload('img', $post_id);
+
+    update_post_meta($post_id, 'img', $photo_id);
+
+    return rest_ensure_response($response);
 }
 
 function register_api_photo_post() {
