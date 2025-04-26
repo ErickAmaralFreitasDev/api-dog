3cecb761294d4c7969dda550e539357bac6c6b9e
E. <123erikalluno123@gmail.com>
2025-04-26 10:01:58 -0300

    reset password 2

--- endpoints/password.php
+++ endpoints/password.php
@@ -12,7 +12,8 @@ function api_password_lost($request) {
         $user = get_user_by('login', $login);
     }
     if(empty($user)) {
-        return new WP_Error('error', 'Usuário não encontrado', ['status' => 401]);
+        $response = new WP_Error('error', 'Usuário não encontrado', ['status' => 401]);
+        return rest_ensure_response($response);
     }
 
     $user_login = $user->user_login;
@@ -47,4 +48,44 @@ function register_api_password_lost() {
 }
 
 add_action('rest_api_init', 'register_api_password_lost');
-?>
+
+//password reset
+
+function api_password_reset($request) {
+    $login = $request['login'];
+    $password = $request['password'];
+    $key = $request['key'];
+
+    if(empty($login) || empty($password) || empty($key)) {
+        return new WP_Error('error', 'Dados incompletos', ['status' => 406]);
+    }
+
+    $user = get_user_by('login', $login);
+
+    if(empty($user)) {
+        $response = new WP_Error('error', 'Usuário não encontrado', ['status' => 401]);
+        return rest_ensure_response($response);
+    }
+
+    $check_key = check_password_reset_key($key, $login);
+
+    if(is_wp_error($check_key)) {
+        $response = new WP_Error('error', 'Token inválido ou expirado', ['status' => 401]);
+        return rest_ensure_response($response);
+    }
+
+    reset_password($user, $password);
+
+    return rest_ensure_response(['message' => 'Senha alterada com sucesso']);
+}
+
+function register_api_password_reset() {
+    register_rest_route('api', '/password/reset', [
+        'methods' => WP_REST_Server::CREATABLE,
+        'callback' => 'api_password_reset',
+    ]);
+}
+
+add_action('rest_api_init', 'register_api_password_reset');
+
+?> 
